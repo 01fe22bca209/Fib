4690db60ebe8b735e27c6666db387c559e05368b
keerthi <01fe22bca209@kletech.ac.in>
2024-03-18 23:32:36 +0530

    code related to fib series

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter the number of terms for Fibonacci series: ");
+        // int n = scanner.nextInt();
+int n=5;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        int firstTerm = 0, secondTerm = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
